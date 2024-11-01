c785a419d3ae98afcd7796866db6aae0adda55ff
William Phan <willdphan@gmail.com>
2024-11-01 13:27:28 -0700

    animate

--- app/components/ProgressItem.tsx
+++ app/components/ProgressItem.tsx
@@ -27,43 +27,67 @@ export const ProgressItem: React.FC<
     switch (type) {
       case "NEXT":
         return (
-          <svg
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             className={iconClass(type)}
           >
-            <path d="M13.0001 16.1716L18.3641 10.8076L19.7783 12.2218L12.0001 20L4.22192 12.2218L5.63614 10.8076L11.0001 16.1716V4H13.0001V16.1716Z"></path>
-          </svg>
+            <motion.path
+              d="M13.0001 16.1716L18.3641 10.8076L19.7783 12.2218L12.0001 20L4.22192 12.2218L5.63614 10.8076L11.0001 16.1716V4H13.0001V16.1716Z"
+              initial={{ y: 0 }}
+              animate={isHovered ? { y: [0, -2, 0] } : { y: 0 }}
+              transition={{
+                duration: 0.3,
+                times: [0, 0.5, 1],
+                ease: "easeInOut",
+              }}
+            />
+          </motion.svg>
         );
       case "ANSWER":
         return (
-          <svg
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             className={iconClass(type)}
+            animate={isHovered ? { scale: 1.1 } : { scale: 1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            <path d="M11.602 13.7599L13.014 15.1719L21.4795 6.7063L22.8938 8.12051L13.014 18.0003L6.65 11.6363L8.06421 10.2221L10.189 12.3469L11.6025 13.7594L11.602 13.7599ZM11.6037 10.9322L16.5563 5.97949L17.9666 7.38977L13.014 12.3424L11.6037 10.9322ZM8.77698 16.5873L7.36396 18.0003L1 11.6363L2.41421 10.2221L3.82723 11.6352L3.82604 11.6363L8.77698 16.5873Z"></path>
-          </svg>
+            <motion.path
+              d="M11.602 13.7599L13.014 15.1719L21.4795 6.7063L22.8938 8.12051L13.014 18.0003L6.65 11.6363L8.06421 10.2221L10.189 12.3469L11.6025 13.7594L11.602 13.7599ZM11.6037 10.9322L16.5563 5.97949L17.9666 7.38977L13.014 12.3424L11.6037 10.9322ZM8.77698 16.5873L7.36396 18.0003L1 11.6363L2.41421 10.2221L3.82723 11.6352L3.82604 11.6363L8.77698 16.5873Z"
+              initial={{ pathLength: 0 }}
+              animate={{ pathLength: isHovered ? 1 : 0 }}
+              transition={{ duration: 0.3 }}
+            />
+          </motion.svg>
         );
       case "HTML":
         return (
-          <svg
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             className={iconClass(type)}
           >
-            <path d="M24 12L18.3431 17.6569L16.9289 16.2426L21.1716 12L16.9289 7.75736L18.3431 6.34315L24 12ZM2.82843 12L7.07107 16.2426L5.65685 17.6569L0 12L5.65685 6.34315L7.07107 7.75736L2.82843 12ZM9.78845 21H7.66009L14.2116 3H16.3399L9.78845 21Z"></path>
-          </svg>
+            <motion.path
+              d="M24 12L18.3431 17.6569L16.9289 16.2426L21.1716 12L16.9289 7.75736L18.3431 6.34315L24 12ZM2.82843 12L7.07107 16.2426L5.65685 17.6569L0 12L5.65685 6.34315L7.07107 7.75736L2.82843 12ZM9.78845 21H7.66009L14.2116 3H16.3399L9.78845 21Z"
+              animate={isHovered ? { rotate: [0, 5, -5, 0] } : { rotate: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+          </motion.svg>
         );
       default:
         return (
-          <svg
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             className={iconClass(type)}
           >
-            <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM13 12H16L12 16L8 12H11V8H13V12Z" />
-          </svg>
+            <motion.path
+              d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM13 12H16L12 16L8 12H11V8H13V12Z"
+              animate={isHovered ? { rotate: [0, 5, -5, 0] } : { rotate: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            />
+          </motion.svg>
         );
     }
   };
